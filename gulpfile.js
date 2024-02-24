a359f1dea3446c9db35691f40fb50c90bf49226c
Franchukkk <sha471251@gmail.com>
2024-02-24 18:28:14 +0200

    header адаптив

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ export const browserSyncFunc = () => {
             baseDir: "docs"
         },
         open: true,
-        browser: "Safari"
+        browser: "Safaari"
         //port: 8080
     })
 }
